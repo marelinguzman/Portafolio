f568addf148bc0cc69e4b0c488194695372a09c9
cristal <mcguzman@scl.edu.gt>
2024-03-06 18:20:49 -0600

    Header

---
+++ js/header_component.js
@@ -0,0 +1,12 @@
+let header = `
+    <div class="logo">MC</div>
+    <nav>
+       <div>Opcion_1</div>
+       <div>Opcion_2</div>
+       <div>Opcion_3</div>
+       <div>Opcion_4</div>
+    </nav>
+    <div class="btn_header">mcguzman</div>
+`;
+
+export { header }
---
+++ js/perfil_component.js
@@ -0,0 +1,7 @@
+let perfil = `
+    <div class="nombre_usuario">
+        <div class="logo1">MC</div>
+        <span class="btn_perfil">mcguzman</span>
+    </div>
+`;
+export { perfil }
