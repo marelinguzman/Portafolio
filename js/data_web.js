28648b7dd218ba97929b93cd4cb63b1e1244a79e
cristal <mcguzman@scl.edu.gt>
2024-03-15 10:38:27 -0600

    Sección de destacados

--- js/data_web.js
+++ js/data_web.js
@@ -1,21 +1,21 @@
 let lista_webs = [
     {
         nombre: "Juego de memoria 📅",
-        img : "https://miro.medium.com/v2/resize:fit:1125/0*N1fmHtI8gmkH_2Vu.png",
+        img : "https://github.com/marelinguzman/img/blob/main/Portafolio/JuegoDeCartas.png?raw=true",
         link_repo : "https://github.com/marelinguzman/Juego-de-memori-2024",
         github_page : "https://marelinguzman.github.io/Juego-de-memoria-2024/",
         descripcion : "Desarrollo de un juego analitico"
     },
     {
         nombre: "EvuPay 📝",
-        img : "https://miro.medium.com/v2/resize:fit:1125/0*N1fmHtI8gmkH_2Vu.png",
+        img : "https://github.com/marelinguzman/img/blob/main/Portafolio/EvuPay.png?raw=true",
         link_repo : "https://github.com/marelinguzman/Ejercicio-3-EvaPay-Home?tab=readme-ov-file",
         github_page : "https://marelinguzman.github.io/Ejercicio-3-EvaPay-Home/",
         descripcion : "Portal de toma de datos de un usuario"
     },
     {
-        nombre: "Transactions 📋",
-        img : "https://miro.medium.com/v2/resize:fit:1125/0*N1fmHtI8gmkH_2Vu.png",
+        nombre: "Versiones de Dispositivos 📋",
+        img : "https://github.com/marelinguzman/img/blob/main/Portafolio/versiones.png?raw=true",
         link_repo : "https://github.com/marelinguzman/Ejercicio-4-Transaction",
         github_page : "https://marelinguzman.github.io/Ejercicio-4-Transaction/",
         descripcion : "Registro de consumo de un usuario"
