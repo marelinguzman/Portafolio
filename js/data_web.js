b174367230cd890bd3cae2ac75d4c7daa0adeac5
mcguzman <mcguzman@scl.edu.gt>
2024-06-22 00:58:26 -0600

    Portafolio - Tienda Virtual

--- js/data_web.js
+++ js/data_web.js
@@ -19,6 +19,34 @@ let lista_webs = [
         link_repo : "https://marelinguzman.github.io/Juego-de-memoria-2024/",
         github_page : "https://github.com/marelinguzman/Juego-de-memoria-2024?tab=readme-ov-file",
         descripcion : "Juego de Memoria para niños de 4 y 5 años"
+    },
+    {
+        nombre: "Clon de Pagina",
+        img : "https://github.com/marelinguzman/img_repositorio/blob/main/Captura%20de%20pantalla%20(93).png?raw=true",
+        link_repo : "https://marelinguzman.github.io/mcdonald-s_pagina/",
+        github_page : "https://github.com/marelinguzman/mcdonald-s_pagina?tab=readme-ov-file",
+        descripcion : "Clon de Pagina Web de McDonal´s"
+    },
+    {
+        nombre: "Indice de Masa Corporal",
+        img : "https://github.com/marelinguzman/img_repositorio/blob/main/Captura%20de%20pantalla%20(94).png?raw=true",
+        link_repo : "https://marelinguzman.github.io/mcdonald-s_pagina/",
+        github_page : "https://github.com/marelinguzman/mcdonald-s_pagina?tab=readme-ov-file",
+        descripcion : "Calculador de Masa Corporal"
+    },
+    {
+        nombre: "Generador de Constraseña",
+        img : "https://github.com/marelinguzman/img_repositorio/blob/main/Captura%20de%20pantalla%20(95).png?raw=true",
+        link_repo : "https://marelinguzman.github.io/mcdonald-s_pagina/",
+        github_page : "https://github.com/marelinguzman/mcdonald-s_pagina?tab=readme-ov-file",
+        descripcion : "Herramienta de un Generador de Contraseña"
+    },
+    {
+        nombre: "Tienda Virtual",
+        img : "https://github.com/marelinguzman/img_repositorio/blob/main/Captura%20de%20pantalla%20(113).png?raw=true",
+        link_repo : "https://marelinguzman.github.io/tienda_virtual/",
+        github_page : "https://github.com/marelinguzman/tienda_virtual?tab=readme-ov-file",
+        descripcion : "Tienda vitual en donde plasmamos todo lo aprendido en programación"
     }
 ];
 
